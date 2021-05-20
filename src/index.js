305ec19570ca6051115dd149f2e67611d82ac96e
KarolisGaiv <kgaivenis@gmail.com>
2021-05-20 16:19:29 +0300

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.scss"
+import _ from "lodash"
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ]
+  }
+};
