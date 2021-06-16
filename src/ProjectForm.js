f3885698e8ede594caff5bab4795e65d87411e70
KarolisGaiv <kgaivenis@gmail.com>
2021-06-16 17:54:59 +0300

    add funcionality to reset project form after submit

--- src/ProjectForm.js
+++ src/ProjectForm.js
@@ -10,12 +10,19 @@ function closeModal() {
   projectModal.classList.remove("is-active");
 }
 
+function clearFields() {
+  projectName.value = "";
+  projectDescription.value = "";
+}
+
 function saveToLocalStorage() {
   let project = [];
   project.push(projectName.value);
   project.push(projectDescription.value);
 
   localStorage.setItem(projectName.value, JSON.stringify(project));
+  clearFields();
+  closeModal();
 }
 
 export { openModal, closeModal, saveToLocalStorage };
